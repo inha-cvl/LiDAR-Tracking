cd8680f7af6da3477b21c5f07da87b15bad920b7
q <rbtjr98@gmail.com>
2024-09-19 12:55:29 +0900

    tracking error solve

--- include/tracking/tracking.hpp
+++ include/tracking/tracking.hpp
@@ -99,53 +99,14 @@ void Tracking::integrationBbox(jsk_recognition_msgs::BoundingBoxArray &cluster_b
     }
     output_bbox_array.boxes.insert(output_bbox_array.boxes.end(), deep_bbox_array.boxes.begin(), deep_bbox_array.boxes.end());
 
+    last_timestamp_cluster = cluster_bbox_array.header.stamp.toSec();
+    last_timestamp_deep = deep_bbox_array.header.stamp.toSec();
+
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     time_taken = elapsed_seconds.count();
     saveTimeToFile(integration_time_log_path, time_taken);
 }
-/*
-void Tracking::cropHDMapBbox(const jsk_recognition_msgs::BoundingBoxArray &input_bbox_array, 
-                             jsk_recognition_msgs::BoundingBoxArray &output_bbox_array, 
-                             const ros::Time &input_stamp, tf2_ros::Buffer &tf_buffer, const std::string target_frame, 
-                             const std::string world_frame, double& time_taken) 
-{
-    auto start = std::chrono::steady_clock::now();
-
-    output_bbox_array.boxes.clear();
-
-    geometry_msgs::TransformStamped transformStamped;
-    try {
-        transformStamped = tf_buffer.lookupTransform(world_frame, target_frame, ros::Time(0)); // input_stamp
-    } catch (tf2::TransformException &ex) {
-        output_bbox_array = input_bbox_array;
-        return;
-    }  
-
-    for (const auto& box : input_bbox_array.boxes) {
-        geometry_msgs::Point transformed_point;
-        tf2::doTransform(box.pose.position, transformed_point, transformStamped);
-
-        bool within_range = false;
-        for (const auto& path_point : global_path) {
-            double distance = std::hypot(path_point.first - transformed_point.x, path_point.second - transformed_point.y);
-            if (distance <= crop_hd_map_radius) {
-                within_range = true;
-                break;
-            }
-        }
-
-        if (within_range) {
-            output_bbox_array.boxes.push_back(box);
-        }
-    }
-
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    time_taken = elapsed_seconds.count();
-    saveTimeToFile(crophdmap_time_log_path, time_taken);
-}
-*/
 
 void Tracking::cropHDMapBbox(const jsk_recognition_msgs::BoundingBoxArray &input_bbox_array, 
                              jsk_recognition_msgs::BoundingBoxArray &output_bbox_array, 
